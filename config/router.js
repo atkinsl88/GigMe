44a833bbf022410ece1a7ca84b8ee14880a94980
Ash Atighetchi <ashgetchi@gmail.com>
2020-08-21 15:58:25 +0100

    router populated

---
+++ config/router.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+const events = require('../controllers/events')
+const auth = require('../controllers/auth')
+const secureRoute = require('../lib/secureRoute')
