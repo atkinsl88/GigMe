2aff943c2ef16b0540dcbb6faf189c3ea3d2e35d
Ash Atighetchi <ashgetchi@gmail.com>
2020-08-21 11:51:48 +0100

    template of our project

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import axios from 'axios'
+
+class App extends React.Component {
+
+  // async componentDidMount() {
+
+  //   try {
+  //     const res = await axios.get('localhost:4000/queens')
+  //     console.log(res.data)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
+  
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+
+export default App
