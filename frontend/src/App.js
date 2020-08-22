21615a0975bf254e129968276bd4136aad0ab38e
Aishath Nasir <aishath_n@hotmail.com>
2020-08-22 14:34:20 +0100

    hooked up props to single gig, index, home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,20 +21,24 @@ class App extends React.Component{
       const res = await axios.get('http://localhost:3000/api/events')
       // console.log(res.data)
       this.setState({ gigs: res.data })
-      console.log(this.state.gigs)
+      // console.log(this.state.gigs)
     } catch (err) {
       console.log(err)
     }
   }
 
 render(){
+
+  console.log(this.state.gigs) //data from API
+
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/gigs/:id" component={GigShow}/>
-        <Route exact path="/gigs" component={GigIndex}/>
+        //! Please make sure if sending props that are all named uniquely here
+        <Route exact path="/" render={(props) => <Home homeProps={this.state.gigs}/>}/>
+        <Route exact path="/gigs/:id" render={(props) => <GigShow singleGigProps={this.state.gigs}/>}/>
+        <Route exact path="/gigs" render={(props) => <GigIndex gigprops={this.state.gigs}/>}/>
         <Route exact path="/community" component={Community}/>
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
