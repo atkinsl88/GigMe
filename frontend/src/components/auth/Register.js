37716c6df72a0fa1243d3e6cfe932d2a265b6bd6
NoaFilosof <noafilosof@hotmail.co.uk>
2020-08-24 21:07:31 +0100

    basic functionality for register interacting with submit (going to api) working

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Select from 'react-select'
+import { registerUser } from '../../lib/api'
+// import Axios from 'axios'
 
 class Register extends React.Component {
 
   state = {
     formData: {
-      userName: '',
+      username: '',
       email: '',
-      genre: '',
+      genres: '',
       aboutMe: '',
       mySite: '',
       profilePicture: '',
@@ -32,39 +34,52 @@ class Register extends React.Component {
 
   //! this function is blatantly untouched JackCode. Would appreciate some help with writing both submit and onchange.
 
-  handleChange = (event) => {
-    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
-    const formData = { ...this.state.formData, [event.target.name]: [event.target.value] }
+  handleChange = event => {
+    // const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+    const formData = { ...this.state.formData, [event.target.name]: event.target.value }
+    // console.log(formData)
     this.setState({ formData })
   }
 
-
+  handleSubmit = async event => {
+    event.preventDefault()
+    try {
+      await registerUser(this.state.formData)
+      console.log(`Welcome ${this.state.formData.username}`)
+    } catch (err) {
+      console.log(err.response.data)
+    }
+  }
   
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label className="label">User Name</label>
         <input
-          type="text"
-          value={this.state.userName}
+          // type="text"
+          name="username"
           onChange={this.handleChange}
+          value={this.state.formData.username}
         />
         <label className="label">Email</label>
       <input
           type="text"
-          value={this.state.email}
+          name="email"
+          value={this.state.formData.email}
           onChange={this.handleChange}
         />
         <label className="label">Website</label>
       <input
           type="text"
-          value={this.state.genre}
+          name="genres"
+          value={this.state.formData.genre}
           onChange={this.handleChange}
         />
         <label className="label">link to profile picture</label>
       <input
           type="text"
-          value={this.state.profilePicture}
+          name="profilePicture"
+          value={this.state.formData.profilePicture}
           onChange={this.handleChange}
         />
         <label className="label">Your favourite genre!</label>
@@ -78,20 +93,22 @@ class Register extends React.Component {
                 <textarea
                   className="textarea"
                   name="description"
-                  value={this.state.aboutMe}
+                  value={this.state.formData.aboutMe}
                   onChange={this.handleChange}
                 />
         </div>
         <label className="label">password</label>
         <input
           type="text"
-          value={this.state.password}
+          name="password"
+          value={this.state.formData.password}
           onChange={this.handleChange}
         />
         <label className="label">confirm password</label>
       <input
           type="text"
-          value={this.state.passwordConfirmation}
+          name="passwordConfirmation"
+          value={this.state.formData.passwordConfirmation}
           onChange={this.handleChange}
         />
         <p>
