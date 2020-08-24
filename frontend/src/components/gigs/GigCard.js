f315e68fc3e7f2cb35586d955661c9c12e920cbd
atkinsl88 <liamatkins24@gmail.com>
2020-08-24 09:47:20 +0100

    remedial changes

--- frontend/src/components/gigs/GigCard.js
+++ frontend/src/components/gigs/GigCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const GigCard = ({ name, image, artistName,venue, date, eventPrice, _id }) => (
+const GigCard = ({ name, image, artistName, venue, date, eventPrice, _id }) => (
 
   <div className="column is-one-quarter-desktop is-one-third-tablet is-fullwidth">
     <Link to={`/gigs/${_id}`}>
