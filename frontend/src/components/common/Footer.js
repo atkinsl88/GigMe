8e1c71a49cbc91b77777df33a093e9c8e7693a6f
NoaFilosof <noafilosof@hotmail.co.uk>
2020-08-27 18:09:23 +0100

    styling changes

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer-main">
-      <div className="footer-text">
-        <h1>Gigme</h1>
-        <h4>made with ♥︎ by Ash, Aishath, Liam and Noa</h4>
+      <div>
+        <h1>GigMe</h1>
+        <h4 className="footer-text">made with ♥︎ by Ash, Aishath, Liam and Noa</h4>
       </div>
     </div>
   )
